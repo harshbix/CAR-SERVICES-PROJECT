16f4c16f27cf027a1fe7c856eaaef46aa620f5f0
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-05-08 08:40:31 -0700

    Update home.jsx

--- car-services/src/pages/home.jsx
+++ car-services/src/pages/home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import '../components/navbar'
 
 const Home = () =>
 {
     <>
         <div>
-            <h4>Welcome to</h4><br/>
+            <h4 className='bg-red'>Welcome to</h4><br/>
              <h2>Find car services near you</h2>
+             <Navbar />
         </div>
     </>
 }
 
-export default Home
+export default Home;
