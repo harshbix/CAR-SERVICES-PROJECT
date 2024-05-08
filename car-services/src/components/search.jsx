fc29440bb1aba2035271b37ed5bc242ac249d883
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-05-08 09:45:14 -0700

    finalizing search btn
    finished the search button

---
+++ car-services/src/components/search.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
+import '../styles/search.css'; // Import your custom CSS file
+
+const Search = () => {
+    return (
+        <div className="search-container py-5 row align-items-center">
+            <div className="input-group col">
+                <div className="input-group-prepend">
+                    <span className="input-group-text bg-black border-black">
+                        <FontAwesomeIcon icon={faSearch} className="text-white" />
+                    </span>
+                </div>
+                <input type="text" className="form-control bg-black border-black text-white" placeholder="Ex : Tire Repair"/>
+            </div>
+            <button type="submit" className="btn col-auto">
+                <FontAwesomeIcon icon={faMapMarkerAlt} className="text-dark bg-white p-3 rounded-2 " />
+            </button>
+        </div>
+    );
+};
+
+export default Search;
--- car-services/src/pages/home.jsx
+++ car-services/src/pages/home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/home.css';
+import Search from '../components/search'
 
 const Home = ({ username }) => {
     return (
@@ -10,6 +11,7 @@ const Home = ({ username }) => {
                 <h4 className='text-white'>Welcome back!</h4>
             )}
             <h2 className='display-3 fw-bold text-white'>Find car services near you</h2>
+            <Search />
         </div>
     );
 };
