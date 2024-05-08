350d47b9bd7f0fd6093f8d87b94be4fe2dfc786c
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-05-08 05:57:41 -0700

    clean improvements

--- car-services/src/App.jsx
+++ car-services/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import './App.css'
-import MyComponent from './mycomponent';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,7 +7,6 @@ function App() {
   return (
     <>
       <div>
-        <h1 className='text-start'>Hello World!</h1>
         <MyComponent />
       </div>  
     </>
