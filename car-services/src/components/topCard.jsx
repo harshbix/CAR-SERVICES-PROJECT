5add5ccf761189e0b7e4b1a9a932333b16912c3c
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-05-09 15:25:08 -0700

    Update topCard.jsx

--- car-services/src/components/topCard.jsx
+++ car-services/src/components/topCard.jsx
@@ -3,10 +3,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faHeart } from '@fortawesome/free-solid-svg-icons';
 import '../styles/topCard.css'; // Import your custom CSS file
 
-const TopCard = ({ imageUrl, name, location, stars }) => {
+const TopCard = ({ name, location, stars }) => {
     return (
         <div className="top-card card">
-            <img src={imageUrl} className="card-img-top" alt="Seller" />
             <div className="card-body">
                 <div className="icon-container">
                     <span className="heart-icon">
