9ab0f1f4793f545925c94d20d8a667bf9dd803bf
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-05-15 15:08:03 -0700

    added a dummy json

--- car-services/src/pages/home.jsx
+++ car-services/src/pages/home.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/home.css';
-import Search from '../components/search'
+import Search from '../components/search';
 import TopCard from '../components/topCard';
 import Navbar from '../components/navbar';
 
-
 const Home = ({ username }) => {
+    const [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        // Fetch user data from users.json
+        fetch('../assets/users.json')
+            .then(response => response.json())
+            .then(data => setUserData(data))
+            .catch(error => console.error('Error fetching user data:', error));
+    }, []);
+
     return (
         <div className='container py-4'>
             {username ? (
@@ -16,8 +25,11 @@ const Home = ({ username }) => {
             <h2 className='display-3 fw-bold text-white'>Find car services near you</h2>
             <Search />
             <div>
-                <h4 className='text-white mb-4'>Top service provider</h4>
-                <TopCard name={"Mwajuma Ibile"} location={"Mbeya"} stars={5}/>
+                <h4 className='text-white mb-4'>Top service providers</h4>
+                {/* Map through user data and render a top card for each user */}
+                {userData.map((user, index) => (
+                    <TopCard key={index} name={user.name} location={user.location} stars={user.stars} />
+                ))}
             </div>
             <Navbar />
         </div>
