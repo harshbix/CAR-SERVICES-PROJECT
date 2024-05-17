4b6786a6dc0e28d77373404206c51514c4237c1b
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-05-16 19:26:48 -0700

    installed material UI

--- car-services/src/App.jsx
+++ car-services/src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import Home from './pages/home';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ProfilePage from './pages/profile';
 
 function App() {
-
   const username = 'Tupac';
-  
+
   return (
-    <>
-      <div className='bg-dark'>
-          <Home username={username}/>
-      </div>  
-    </>
-  )
+    <div className='bg-dark'>
+      {/* Uncomment this line if you want to render the Home component */}
+      {/* <Home username={username} /> */}
+      <ProfilePage />
+    </div>
+  );
 }
 
-export default App
+export default App;
