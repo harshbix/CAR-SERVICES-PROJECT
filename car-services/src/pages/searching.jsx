5dd96f066a0ebb13dba38522882e129dc26f367d
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-05-23 22:45:24 -0700

    Update searching.jsx

--- car-services/src/pages/searching.jsx
+++ car-services/src/pages/searching.jsx
@@ -7,6 +7,9 @@ function SearchPage() {
         <> 
         <div className='bg-dark p-3 vh-100'>
             <Search />
+            <p className="text-white text-center display-5 text-muted">
+                Enter Location, and get results here!
+            </p>
             <Navbar />
         </div>
         </>
