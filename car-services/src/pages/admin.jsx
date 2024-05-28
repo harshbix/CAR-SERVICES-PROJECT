5c06fa2b496abfba980fd23be8bb73462def4516
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-05-28 16:06:50 -0700

    starter for mechanics and admin
    few improvements on admin and mechanics panel

---
+++ car-services/src/pages/admin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Admin = () => 
+    {
+        return (
+            <>
+            <h1>Hello, Admin</h1>
+            </>
+        )
+    }
+
+    export default Admin;
---
+++ car-services/src/pages/mechanics.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Mechanics = () => 
+    {
+        return (
+            <>
+            <h1>Hello, Mechanics</h1>
+            </>
+        )
+    }
+
+export default Admin;
