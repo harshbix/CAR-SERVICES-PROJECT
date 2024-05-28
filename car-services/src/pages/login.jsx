2d3a7a97ffa0e673d116295250b02dc5d2312230
Junior Jeconia <116210510+harshbix@users.noreply.github.com>
2024-05-28 16:23:36 -0700

    Update login.jsx
    w3 schools login import

--- car-services/src/pages/login.jsx
+++ car-services/src/pages/login.jsx
@@ -4,7 +4,24 @@ const Login = () =>
     {
         return (
             <>
-            <h1>Here is Log in</h1>
+            <form action="/action_page.php">
+                <div class="mb-3 mt-3">
+                    <label for="email" class="form-label">Email:</label>
+                    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email"></input>
+                </div>
+                <div class="mb-3">
+                    <label for="pwd" class="form-label">Password:</label>
+                    <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd"></input>
+                </div>
+                <div class="form-check mb-3">
+                    <label class="form-check-label">
+                    <input class="form-check-input" type="checkbox" name="remember">  </input>Remember me
+                    </label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
             </>
         )
     }
+
+    export default Login;
